5e2a00159dd766381f39942ea2f5390d9ec104e0
Dev-kid-Dolo <robinsonwesley206@gmail.com>
2026-09-21 16:37:15 -0400

    Create GameMap.jsx

---
+++ GameMap.jsx
@@ -0,0 +1,27 @@
+const tiles = [
+  ["🌲", "🌲", "🌲", "🌲", "🌲"],
+  ["🌲", "⬜", "⬜", "⬜", "🌲"],
+  ["🌲", "⬜", "🧙", "⬜", "🌲"],
+  ["🌲", "⬜", "⬜", "⬜", "🌲"],
+  ["🌲", "🌲", "🌲", "🌲", "🌲"]
+];
+
+export default function GameMap() {
+  return (
+    <div>
+      {tiles.map((row, y) => (
+        <div key={y}>
+          {row.map((tile, x) => (
+            <span key={x} style={{ fontSize: "2rem" }}>
+              {tile}
+            </span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+Import into App:
+import GameMap from "./GameMap";
+
+<GameMap />
