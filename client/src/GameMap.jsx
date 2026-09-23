7a41ff7fccae2b3ab2dd6d4ce6e6b44973b195f2
Dev-kid-Dolo <robinsonwesley206@gmail.com>
2026-09-22 18:19:03 -0700

    Update GameMap.jsx

--- client/src/GameMap.jsx
+++ client/src/GameMap.jsx
@@ -8,11 +8,23 @@ const tiles = [
 
 export default function GameMap() {
   return (
-    <div>
+    <div
+      style={{
+        padding: "20px",
+        fontFamily: "monospace",
+      }}
+    >
+      <h1>🧙 Grim Wizard</h1>
+
       {tiles.map((row, y) => (
         <div key={y}>
           {row.map((tile, x) => (
-            <span key={x} style={{ fontSize: "2rem" }}>
+            <span
+              key={x}
+              style={{
+                fontSize: "2rem",
+              }}
+            >
               {tile}
             </span>
           ))}
